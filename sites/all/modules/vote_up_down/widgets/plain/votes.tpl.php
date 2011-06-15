6501af28fc56aaaea9bf00d3b6994c9ea563ef8a
sumeshcitrus <sumesh@citrusinformatics.com>
2011-06-15 15:31:35 +0530

    For correcting the voting icon misalignment.

--- sites/all/modules/vote_up_down/widgets/plain/votes.tpl.php
+++ sites/all/modules/vote_up_down/widgets/plain/votes.tpl.php
@@ -7,4 +7,18 @@
  * Plain widget votes display for Vote Up/Down
  */
 ?>
-<span id="<?php print $id; ?>" class="total-votes-plain"><span class="<?php print $class; ?> total"><?php print $up_votes; ?>&nbsp;votes&nbsp;<?php print $down_votes; ?>&nbsp;&nbsp;dislikes<?php //print $vote_label; ?></span></span>
+<span id="<?php print $id; ?>" class="total-votes-plain"><span class="<?php print $class; ?> total"><?php print $up_votes; ?>&nbsp;votes&nbsp;<?php print $down_votes; ?>&nbsp;&nbsp;dislikes<?php //print $vote_label; ?>
+<span  ></span>&nbsp;<span  style="font-size:13px;font-family:tahoma;"></span>
+
+  <a href="<?php print $link_up; ?>" class="<?php print $link_class_up; ?>">
+      <span class="<?php print $class_up; ?>" title="<?php print t('Vote up!'); ?>"></span>
+    </a>
+    <a href="<?php print $link_down; ?>" class="<?php print $link_class_down; ?>">
+      <span class="<?php print $class_down; ?>" title="<?php print t('Vote down!'); ?>">  </span>
+    </a>
+	<span  style="font-size:12px;font-family:tahoma;"></span>
+	
+
+
+
+</span></span>
