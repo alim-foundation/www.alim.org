3837e3bf34bea150670d98cc2e41b975c7a35e0e
sumeshcitrus <sumesh@citrusinformatics.com>
2011-07-19 15:29:40 +0530

    Ticket #94: Given the dimensions to home page recent activity, user images. For improving the page speed performance.

--- sites/all/themes/alim/views-view-field--recent-comments-new--name-1.tpl.php
+++ sites/all/themes/alim/views-view-field--recent-comments-new--name-1.tpl.php
@@ -7,17 +7,17 @@ $temp_user = user_load(array('name' => strip_tags($output)));
 $picture = $temp_user->picture; 
 ?>
 <a href="<?=$base_url?>/userprofile/<?=strip_tags($output)?>" class="gotouser">
-<div id="user_image">
+<div id="user_image" style="width:35px">
 
 <?php if($picture!="") { ?>
 <img  src="<?=$base_url?>/<?=$picture?>"  height="24" width="24" align="absmiddle" alt="image" />
 <?php 
 } 
  else if($temp_user->rpx_data['profile']['photo']!="" && $picture=="") {?>
-<img height="24px"  width="24px" src="<?php print $temp_user->rpx_data['profile']['photo']; ?>" align="absmiddle" />
+<img height="24"  width="24" src="<?php print $temp_user->rpx_data['profile']['photo']; ?>" align="absmiddle" />
 <?php 
 } 
- else if($temp_user->rpx_data['profile']['photo']=="" && $picture=="") {?><img src='http://alim.org/sites/all/themes/alim/images/photobg.png' height="24px"  width="24px" align="absmiddle" />
+ else if($temp_user->rpx_data['profile']['photo']=="" && $picture=="") {?><img src='http://alim.org/sites/all/themes/alim/images/photobg.png' height="32"  width="32" align="absmiddle" />
 <?php } ?>
 </div>
 <div  style="float:left;padding-top:7px;padding-bottom:7px;width:80px;" > 
