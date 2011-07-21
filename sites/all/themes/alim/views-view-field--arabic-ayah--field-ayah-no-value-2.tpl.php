8404033b0dcb6240044cc045936153a3a5ce2b9c
asiacitrus <asia@citrusinformatics.com>
2011-07-21 15:51:56 +0530

    Given height and width for the image

--- sites/all/themes/alim/views-view-field--arabic-ayah--field-ayah-no-value-2.tpl.php
+++ sites/all/themes/alim/views-view-field--arabic-ayah--field-ayah-no-value-2.tpl.php
@@ -5,6 +5,6 @@ global $theme_path;
 ?>
 <!--<div style="width:24px;padding-top:10px;">-->
 <div id="arbarrow_<?php print strip_tags($output); ?>" style="display:none;width:24px;padding-top:10px;" class="arbarrow">
-<img src="<?=$base_url?>/<?=$theme_path?>/images/icon_arrow_green.gif" />
+<img src="<?=$base_url?>/<?=$theme_path?>/images/icon_arrow_green.gif"  width="24" height="24"/>
 </div>
 <!--</div>-->
