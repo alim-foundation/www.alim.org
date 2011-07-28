70ebd5b83013d9d610bc3b04acbf37e4450503f2
sumeshcitrus <sumesh@citrusinformatics.com>
2011-07-28 13:19:53 +0530

    Corrected change font option bug in tafsir pages.

--- sites/all/themes/alim/page.tpl.php
+++ sites/all/themes/alim/page.tpl.php
@@ -871,7 +871,7 @@ if(arg(3)=='compare') {
 							
 							<div id="content_data">	
 							
-							<?php if(arg(3)=='english' || arg(3)=='compare') { ?> <div class="font_ayah" id="font_ayah">  <?php  } ?>
+							<?php if(arg(3)=='english' || arg(3)=='compare' || arg(2)=='AlQuran-tafsir') { ?> <div class="font_ayah" id="font_ayah">  <?php  } ?>
 							
 							<?php if(arg(0)=='userprofile' || arg(0)=='groupdetails') { ?>
 								<div>
