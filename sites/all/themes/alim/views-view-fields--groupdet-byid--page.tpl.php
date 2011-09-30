e040a8cf668380900ff7ac23c3bd35b251591ec7
sumeshcitrus <sumesh@citrusinformatics.com>
2011-09-30 16:08:17 +0530

    Ticket #496: Invite users to group - Modifications

--- sites/all/themes/alim/views-view-fields--groupdet-byid--page.tpl.php
+++ sites/all/themes/alim/views-view-fields--groupdet-byid--page.tpl.php
@@ -83,7 +83,7 @@ else
 					
 		    $headers["MIME-Version"] = '1.0';
 		    $headers["Content-Type"] = "text/html; charset=iso-8859-1";
-		    $headers["From"] = "$email_name <$from>";
+		    $headers["From"] = "Alim.org <$from>";
 			
 			// Mail it
 
@@ -110,7 +110,8 @@ else
 		    $headers["MIME-Version"] = '1.0';
 		    $headers["Content-Type"] = "text/html; charset=iso-8859-1";
 		    $headers["From"] = "Alim.org <$from>";
-						
+			
+			
 			// Mail it
 			/*if ($email_sent == 'false') {
 			  mail($val, $subject, $message, $headers);
