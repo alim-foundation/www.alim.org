2afa3758f9d69c2a39c6cee25c61c3d2134968cb
asiacitrus <asia@citrusinformatics.com>
2012-09-17 09:40:17 +0530

    updated

--- sites/all/themes/alim/clipboard-hand.js
+++ sites/all/themes/alim/clipboard-hand.js
@@ -136,7 +136,7 @@ else if (e) keycode = e.which;
 	 {
 	  setTimeout(function(){
 						  var ayahend=$('.copy-to').val();
-						  alert(ayahend);
+						  //alert(ayahend);
 						  if(parseInt(ayahstart) > parseInt(ayahend)){
 						  $('.copy-to').val(ayahstart);}}, 2000);
 	 }
