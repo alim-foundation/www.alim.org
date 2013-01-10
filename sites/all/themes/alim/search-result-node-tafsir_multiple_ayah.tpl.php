a3cbed95c846640176c79868575de887ea76ee5a
asiacitrus <asia@citrusinformatics.com>
2013-01-10 09:13:22 +0530

    updated

--- sites/all/themes/alim/search-result-node-tafsir_multiple_ayah.tpl.php
+++ sites/all/themes/alim/search-result-node-tafsir_multiple_ayah.tpl.php
@@ -52,7 +52,7 @@
 <dt class="title">
 
 <?php 
-print_r($node_result);
+print_r($node_result->field_book_code_multiple[0]['value']);
 // create url and title of search result
 $urlto = 'library/quran/AlQuran-tafsir/TIK/'.$node_result->field_tafsir_surah_number[0]['value'].'/'.$node_result->field_tafsir_ayah_from[0]['value'] ;
 $titletxt =$node_result->title; 
