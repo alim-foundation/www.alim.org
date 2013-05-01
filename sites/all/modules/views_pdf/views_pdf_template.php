f65983234cacc68b8ce0c3449ca0c55f93c8fb19
asia@citrusinformatics.com <asia@citrusinformatics.com>
2013-05-01 18:15:42 +0530

    updated

--- sites/all/modules/views_pdf/views_pdf_template.php
+++ sites/all/modules/views_pdf/views_pdf_template.php
@@ -724,7 +724,7 @@ $titl= "<br/><h3>".arg(8)." - ".$sec_head."</h3>";
     }
     
     $this->SetY($rowY + $options['position']['row_height']);
-     $this->renderRow($x, $y, "Alim.org",""); 
+     $this->renderRow($x, $y, "Alim.org",$headerOptions); 
     
   }
   
