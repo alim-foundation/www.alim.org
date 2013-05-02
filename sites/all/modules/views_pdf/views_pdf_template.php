9ce2be872b42d5de39c0053e2ff5109a8b93346c
asia@citrusinformatics.com <asia@citrusinformatics.com>
2013-05-02 13:23:14 +0530

    updated

--- sites/all/modules/views_pdf/views_pdf_template.php
+++ sites/all/modules/views_pdf/views_pdf_template.php
@@ -546,7 +546,9 @@ $titl= "<br/><h3>".arg(8)." - ".$sec_head."</h3>";
 	//footer
      if($row=="Alim.org")
 	 {
-			 $h+50;
+			 $h+=50;
+			 $x+=30;
+			 $y+=30;
 	 }
     // Write the content of a field to the pdf file:
     $this->MultiCell($w, $h, $content, $border, $align, $fill, $ln, $x, $y, $reseth, $stretch, $ishtml, $autopadding, $maxh, $valign, $fitcell);
