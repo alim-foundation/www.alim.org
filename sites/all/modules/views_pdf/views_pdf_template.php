fed4790ab5d9f73bd91f71d3057adda5f7aedc18
asia@citrusinformatics.com <asia@citrusinformatics.com>
2013-05-07 17:55:40 +0530

    updated comment display

--- sites/all/modules/views_pdf/views_pdf_template.php
+++ sites/all/modules/views_pdf/views_pdf_template.php
@@ -444,7 +444,7 @@ $titl= "<br/><h3>".arg(8)." - ".$sec_head."</h3>";
 	 if($row=="Alim.org")
 	 {
 	 if(arg(5)=="PIK" || arg(5)=="ASD" || arg(5)=="YAT" || arg(5)=="MAL" || arg(3)=="compare"){
-	 $coment= views_embed_view('ayahelaboration_bottom', 'block_2', array(arg(4),arg(5)));
+	 $coment= views_embed_view('ayahelaboration_bottom', 'defaults', array(arg(4),arg(5)));
 	 }
 	 $titl="";
 	 $logo="";
