14e0d78c1f405563705f1de96e8fffd3e189c3de
asia@citrusinformatics.com <asia@citrusinformatics.com>
2013-05-07 17:37:18 +0530

    updated for showing superscript number

--- sites/all/modules/views_pdf/views_pdf_template.php
+++ sites/all/modules/views_pdf/views_pdf_template.php
@@ -553,6 +553,9 @@ $titl= "<br/><h3>".arg(8)." - ".$sec_head."</h3>";
     
                 
     // Write the content of a field to the pdf file:
+	$searchhtmlsup = array("<span class='fn'>", "</span>");
+	$replaceoriginalsup = array('<sup style="color:red">', '</sup>');
+	$content = str_replace($searchhtmlsup, $replaceoriginalsup, $content);
     $this->MultiCell($w, $h, $content, $border, $align, $fill, $ln, $x, $y, $reseth, $stretch, $ishtml, $autopadding, $maxh, $valign, $fitcell);
     
     // Reset font to default
