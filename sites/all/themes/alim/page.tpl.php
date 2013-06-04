803604825a0421729ee643e2ffdaff622ab669eb
asia@citrusinformatics.com <asia@citrusinformatics.com>
2013-06-04 14:48:46 +0530

    updated

--- sites/all/themes/alim/page.tpl.php
+++ sites/all/themes/alim/page.tpl.php
@@ -811,7 +811,7 @@ if(arg(3)=='compare') {
 				<div style="clear:both"></div>
 			</div>
 		
-			<div class="inner_donate">
+			<div class="inner_donate">  
 						<div> <a href="https://donatenow.networkforgood.org/alim?code=alim.org" target="_blank" ><div style="margin-top:-18px;float: right;width: 200px; "> <img  src="/sites/all/themes/alim/images/donate-button.png" /></div></a></div>
 			</div>			
 		
