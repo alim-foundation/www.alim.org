850e80e65f1a127e416990a6bb5bfe405ba9de6b
asia@citrusinformatics.com <asia@citrusinformatics.com>
2013-06-07 14:53:30 +0530

    added for list of users to download

---
+++ sites/all/themes/alim/page-node-187924.tpl.php
@@ -0,0 +1,36 @@
+<?php
+ini_set('max_execution_time', 3000);
+header('Pragma: public');
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");   
+header('Last-Modified: '.gmdate('D, d M Y H:i:s') . ' GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: pre-check=0, post-check=0, max-age=0');
+header("Pragma: no-cache");
+header("Expires: 0");
+header('Content-Transfer-Encoding: none');
+header('Content-Type: application/vnd.ms-excel;');
+header("Content-type: application/x-msexcel");
+header('Content-Disposition: attachment; filename=Noorvitamins Orders.xls');
+$a=$_GET['a'];
+$b=$_GET['b'];
+$a = date("Y-m-d", strtotime($a));
+$b = date("Y-m-d", strtotime($b));
+
+if($a=="1969-12-31" && $b=="1969-12-31")
+$qry_string="FROM_UNIXTIME(created) >=DATE_SUB(CURDATE(), INTERVAL 1 MONTH)";
+else
+$qry_string="DATE(FROM_UNIXTIME(created)) BETWEEN CONCAT('".$a ."',' ','00:00:00') AND CONCAT('".$b ."',' ','23:59:59')  ";
+
+echo "<table><tr style='border:2px solid #666666'><th align='left'>NAME</th><th align='left'>EMAIL</th></tr>";  
+$query1=db_query('SELECT name as names,mail as email FROM `users` WHERE  '.$qry_string.' ORDER BY name DESC');
+while ($row1 = db_fetch_object($query1)) 
+{
+	print '<tr style="border-bottom:1px solid #CCCCCC;">
+	<td align="left" style="border-right:1px solid #CCCCCC; ">'. $row1->names.'</td>
+	<td align="left" style="border-right:1px solid #CCCCCC; ">'. $row1->email.'</td>';
+    print $inner.'</tr>';
+  
+}
+print '</table>';
+?>
+
