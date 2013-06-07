94825faae6bb8562169f009188a65e85c80dbf2a
asia@citrusinformatics.com <asia@citrusinformatics.com>
2013-06-07 18:04:49 +0530

    updated

--- sites/all/themes/alim/page-node-187924.tpl.php
+++ sites/all/themes/alim/page-node-187924.tpl.php
@@ -22,11 +22,11 @@ else
 $qry_string="DATE(FROM_UNIXTIME(created)) BETWEEN CONCAT('".$a ."',' ','00:00:00') AND CONCAT('".$b ."',' ','23:59:59')  ";
 
 echo "<table><tr style='border:2px solid #666666'><th align='left'>USER NAME</th><th align='left'>FIRST NAME</th><th align='left'>EMAIL</th></tr>";  
-$query1=db_query('SELECT  users.uid, `name` ,mail ,profile_values.value as fname  FROM `users`,profile_values WHERE  '.$qry_string.' AND profile_values.uid = users.uid  GROUP BY uid DESC');
+$query1=db_query('SELECT  users.uid, `name`  ,mail as email ,profile_values.value as fname  FROM `users`,profile_values WHERE  '.$qry_string.' AND profile_values.uid = users.uid  GROUP BY uid DESC');
 while ($row1 = db_fetch_object($query1)) 
 {
 	print '<tr style="border-bottom:1px solid #CCCCCC;">
-	<td align="left" style="border-right:1px solid #CCCCCC; ">'. $row1->names.'</td>
+	<td align="left" style="border-right:1px solid #CCCCCC; ">'. $row1->name.'</td>
 	<td align="left" style="border-right:1px solid #CCCCCC; ">'. $row1->fname.'</td>
 	<td align="left" style="border-right:1px solid #CCCCCC; ">'. $row1->email.'</td>
 	';
