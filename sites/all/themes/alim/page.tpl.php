c562c46c96a937918f5807d9cc026fff2ab0a65a
asia@citrusinformatics.com <asia@citrusinformatics.com>
2013-06-07 16:59:45 +0530

    updated

--- sites/all/themes/alim/page.tpl.php
+++ sites/all/themes/alim/page.tpl.php
@@ -413,7 +413,7 @@ $start_time = $_SESSION['start_time'];
 <link rel="stylesheet" type="text/css" href="<?=$base_url?>/<?=$theme_path?>/jqlist/prettify.css" />
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
 <link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/ui-lightness/jquery-ui.css" />
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js" type="text/javascript"></script>
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
   <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
   <script>
   var jq = jQuery.noConflict();
