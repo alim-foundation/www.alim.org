36505b2297d10bdbad049534abe8d23120b536ec
asia@citrusinformatics.com <asia@citrusinformatics.com>
2013-07-05 18:47:07 +0530

    updated

--- sites/all/themes/alim/webform-mail-163634.tpl.php
+++ sites/all/themes/alim/webform-mail-163634.tpl.php
@@ -1,10 +1,13 @@
 <?php
 print "Recieved a request to upgrade the role. Please see the details:"."\n\n";
-
+$link="http://alim.org/node/163634/webform-results";
 print "Name				:" .$form_values['submitted'][3]."\n";
 print "E-mail			:" .$form_values['submitted'][1]."\n";
 print "Message			:" .$form_values['submitted'][2]."\n";
 print "Upgrade role to	:" .$form_values['submitted'][4]."\n\n\n";
+
+print "The results of this submission may be viewed at:\n";
+print  $link;
 
 print "Alim.org";
 ?>
