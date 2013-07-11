df956e1b6cfed283b85364e7709b4f2db75ee5f8
asia@citrusinformatics.com <asia@citrusinformatics.com>
2013-07-11 12:52:19 +0530

    updated

--- sites/all/themes/alim/page.tpl.php
+++ sites/all/themes/alim/page.tpl.php
@@ -809,6 +809,10 @@ if(arg(3)=='compare') {
 					</div>
 					<div style="float:left;"><h1> <?php print print_masterhead(); ?>  </h1></div>		
 				</div>
+				<div class="inner_donate">  
+						<div> <a href="https://donatenow.networkforgood.org/alim?code=alim.org" target="_blank" ><div style="margin-top: 15px;margin-left: -50px;float: right;width: 200px; "> <img  src="/sites/all/themes/alim/images/donate-button.png"  width="230" /></div></a></div>
+			</div>			
+
 				<div id="search" align="left"><div id="search_content"><?php if ($search_box): ?><div ><?php print $search_box ?></div><?php endif; // search box  ?></div></div>
 			</div>
 			<div style="clear:both"></div>
@@ -817,12 +821,7 @@ if(arg(3)=='compare') {
 				<?php print Submenu_all(); ?>
 				<?php if ($menu_tab): print $menu_tab; endif; ?>
 				<div style="clear:both"></div>
-			</div>
-		
-			<div class="inner_donate">  
-						<div> <a href="https://donatenow.networkforgood.org/alim?code=alim.org" target="_blank" ><div style="margin-top: -3px;margin-left: -5px;float: right;width: 200px; "> <img  src="/sites/all/themes/alim/images/donate-button.png"  width="200" /></div></a></div>
-			</div>			
-		
+			</div>		
 		 </div>
 		<div style="clear:both"></div>
 			<div id="con_div">
@@ -963,6 +962,9 @@ if(arg(3)=='compare') {
 	<div id="footer_div" align="center" >
 	  <div id="footer_content" align="center">
 		  <div id="footer_head" align="center"><a  href="<?=$base_path?>" title="Home"><img src="<?php print $base_url."/".$theme_path;?>/images/alim-logo1.png" border="0"  width="262" height="61" /></a><br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&copy; All Rights 2010 Alim.org</div>
+		  <div class="inner_donate">  
+						<div> <a href="https://donatenow.networkforgood.org/alim?code=alim.org" target="_blank" ><div style="margin-top: 10px;margin-left: -5px;float: right;width: 200px; "> <img  src="/sites/all/themes/alim/images/donate-button.png"  width="230" /></div></a></div>
+			</div>
 		  
 		  <div id="footer_menu"><?php print $footer; ?></div>
 	  </div>
